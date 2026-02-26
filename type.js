d1f150f00d187f68c341a1217e198700cd635bfd
Samhita <samhitatavutu@gmail.com>
2026-02-26 17:01:34 +0530

    Refactor typeText function and move text declarations

--- type.js
+++ type.js
@@ -1,28 +1,32 @@
 const heading = document.getElementById("heading");
 const subheading = document.getElementById("subheading");
 
-const headingText = "Hi, I'm Samhita.";
-const subheadingText = "Cybersecurity enthusiast building secure systems.";
+if (heading && subheading) {
 
-function typeText(element, text, speed, callback) {
-  let i = 0;
+  const headingText = "Hi, I'm Samhita.";
+  const subheadingText = "Cybersecurity enthusiast building secure systems.";
 
-  function typing() {
-    if (i < text.length) {
-      element.innerHTML += text.charAt(i);
-      i++;
-      setTimeout(typing, speed);
-    } else if (callback) {
-      callback();
+  function typeText(element, text, speed, callback) {
+    let i = 0;
+
+    function typing() {
+      if (i < text.length) {
+        element.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, speed);
+      } else if (callback) {
+        callback();
+      }
     }
+
+    typing();
   }
 
-  typing();
-}
+  typeText(heading, headingText, 100, () => {
+    typeText(subheading, subheadingText, 50);
+  });
 
-typeText(heading, headingText, 100, () => {
-  typeText(subheading, subheadingText, 50);
-});
+}
 // Hamburger functionality
 const hamburger = document.querySelector(".hamburger");
 const sidePanel = document.getElementById("sidePanel");
